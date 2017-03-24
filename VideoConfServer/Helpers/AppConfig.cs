0889fc512992bcc3f9690b65bdfca4479d10b7b5
Yakiv Yusin <yusin.yakiv@gmail.com>
2017-03-24 15:10:43 +0200

    App.config + AppConfig Wrapper

---
+++ VideoConfServer/Helpers/AppConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Configuration;
+
+namespace VideoConfServer.Helpers
+{
+    /// <summary>
+    /// Wrapper class for working with App.config
+    /// </summary>
+    public static class AppConfig
+    {
+        /// <summary>
+        /// Base server endpoint
+        /// </summary>
+        public static string ServerEndpoint
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["ServerEndpoint"];
+            }
+        }
+
+        /// <summary>
+        /// Path for main image endpoint (without rotations)
+        /// </summary>
+        public static string MainPath
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["MainPath"];
+            }
+        }
+    }
+}
