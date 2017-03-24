8c7ba9f161e49496da0969f138788231f779636b
Yakiv Yusin <yusin.yakiv@gmail.com>
2017-03-24 11:03:53 +0200

    Init project

---
+++ VideoConfServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoConfServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
